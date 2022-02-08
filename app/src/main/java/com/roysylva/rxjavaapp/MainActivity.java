d05176ef1e2bfe2e02b185e94de957ea8cda3c68
Roy Sylvanus <vicevanus@gmail.com>
2022-02-08 11:36:35 +0100

    More Rx Operators

--- app/src/main/java/com/roysylva/rxjavaapp/MainActivity.java
+++ app/src/main/java/com/roysylva/rxjavaapp/MainActivity.java
@@ -2,18 +2,27 @@ package com.roysylva.rxjavaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.TestLooperManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.observers.DisposableObserver;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -23,20 +32,33 @@ public class MainActivity extends AppCompatActivity {
     //here we create a string and set it as an observable item
     private String greeting = "Hello from RxJAva";
 
-    private Observable<String> myObservable;
+  //  private Observable<String> myObservable;
 
     //then we create an observer for the observable
     //we can always create more than one observer for an observable
-    private DisposableObserver<String> myObserver;
+//    private DisposableObserver<String> myObserver;
 
     private DisposableObserver<String> myObserver2;
     private static final String TAG = "MainActivity";
     private TextView textView;
+    private Button button;
    // private Disposable disposable;
 
 
     //composite disposable helps to clear all subscriptions when you have more than one observer
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+
+
+    //from array
+
+    private String[] array = {"C","B"};
+
+
+
+    //create operator
+    private Observable<Student> myObservable;
+    private DisposableObserver<Student> myObserver;
 
 
     @Override
@@ -45,12 +67,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView)findViewById(R.id.tvView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MoreRxOperations.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //create operator
+
+        myObservable = Observable.create(new ObservableOnSubscribe<Student>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Student> emitter) throws Throwable {
+                ArrayList<Student> studentsArray = getStudents();
+
+                for (Student student:studentsArray){
+                    emitter.onNext(student);
+                }
+                emitter.onComplete();
+            }
+        });
 
         //just operator converts an item to observable
-        myObservable = Observable.just(greeting);
+   //     myObservable = Observable.just(greeting);
 
 
-                //observer with no disposabele interface
+        //range operator
+   //     myObservable = Observable.range(1,20)
+
+
+        //from array creates an observable of array type
+     //   myObservable = Observable.fromArray(array)
+
+                //observer with no disposable interface
 
        /* myObserver = new Observer<String>() {
             @Override
@@ -83,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
         };
 */
         //observer with disposable interface
-        myObserver = new DisposableObserver<String>() {
+        /*myObserver = new DisposableObserver<String>() {
             @Override
             public void onNext(@NonNull String s) {
 
@@ -100,14 +153,14 @@ public class MainActivity extends AppCompatActivity {
             public void onComplete() {
                 Log.e(TAG,"on complete invoked");
             }
-        };
+        };*/
 
         //here we add observers to the composite disposable
-        compositeDisposable.add(
+     /*   compositeDisposable.add(
         myObservable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(myObserver));
-
+*/
 
 
         myObserver2 = new DisposableObserver<String>() {
@@ -129,15 +182,30 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        compositeDisposable.add(
+       /* compositeDisposable.add(
                 myObservable.subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(myObserver2));
+                        .subscribeWith(myObserver2));*/
+
+
+        //map operator
 
         compositeDisposable.add(
                 myObservable.subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
+                        .map(new Function<Student ,Student>() {
+                            @Override
+                            public Student apply(Student student) throws Throwable {
+
+
+
+                                student.setName(student.getName().toUpperCase());
+                                return student;
+                            }
+                        })
                         .subscribeWith(getObserver()));
+
+
     }
 
     @Override
@@ -159,7 +227,7 @@ But if you call to clear() method you can still add disposable to the composite 
     }
 
 
-    private DisposableObserver getObserver(){
+   /* private DisposableObserver getObserver(){
         myObserver = new DisposableObserver<String>() {
             @Override
             public void onNext(@NonNull String s) {
@@ -180,5 +248,55 @@ But if you call to clear() method you can still add disposable to the composite 
         };
 
         return myObserver;
+    }*/
+
+     private DisposableObserver getObserver(){
+            myObserver = new DisposableObserver<Student>() {
+                @Override
+                public void onNext(@NonNull Student s) {
+
+                    Log.e(TAG,"on next invoked" + s.getEmail());
+
+                }
+
+                @Override
+                public void onError(@NonNull Throwable e) {
+                    Log.e(TAG,"on error invoked");
+                }
+
+                @Override
+                public void onComplete() {
+                    Log.e(TAG,"on complete invoked");
+                }
+            };
+
+            return myObserver;
+        }
+    private ArrayList<Student> getStudents(){
+
+        ArrayList<Student> students = new ArrayList<>();
+
+        Student student = new Student();
+        student.setName("Mike");
+        student.setAge(5);
+        student.setEmail("a@mail.com");
+        student.setRegistrationDate("5th March");
+        students.add(student);
+
+        Student student1 = new Student();
+        student1.setName("Mike");
+        student1.setAge(5);
+        student.setEmail("a@mail.com");
+        student1.setRegistrationDate("5th March");
+        students.add(student1);
+
+        Student student2 = new Student();
+        student2.setName("Mike");
+        student2.setAge(5);
+        student.setEmail("a@mail.com");
+        student2.setRegistrationDate("5th March");
+        students.add(student2);
+
+        return students;
     }
 }
---
+++ app/src/main/java/com/roysylva/rxjavaapp/MoreRxOperations.java
@@ -0,0 +1,162 @@
+package com.roysylva.rxjavaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.ObservableSource;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.observers.DisposableObserver;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MoreRxOperations extends AppCompatActivity {
+    private String greeting = "Hello from RxJAva";
+    private DisposableObserver<String> myObserver2;
+    private static final String TAG = "MoreRxOperations";
+    private TextView textView;
+
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private String[] array = {"C","B"};
+
+
+
+    //create operator
+    private Observable<Student> myObservable;
+    private DisposableObserver<Student> myObserver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_rx_operations);
+        textView = (TextView)findViewById(R.id.tvView2);
+        myObservable = Observable.create(new ObservableOnSubscribe<Student>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Student> emitter) throws Throwable {
+                ArrayList<Student> studentsArray = getStudents();
+
+                for (Student student:studentsArray){
+                    emitter.onNext(student);
+                }
+                emitter.onComplete();
+            }
+        });
+
+        compositeDisposable.add(
+                myObservable.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+
+                        //map
+
+                        /*.map(new Function<Student ,Student>() {
+                            @Override
+                            public Student apply(Student student) throws Throwable {
+                                student.setName(student.getName().toUpperCase(Locale.ROOT));
+                                return student;
+                            }
+                        })*/
+
+                        //flatmap operator
+
+                       /* .flatMap(new Function<Student, ObservableSource<?>>() {
+                            @Override
+                            public Observable<Student> apply(Student student) throws Throwable {
+
+                                Student student1 = new Student();
+                                student1.setName("Tom");
+
+                                Student student2 = new Student();
+                                student2.setName("Francis");
+
+                                student.setName(student.getName().toUpperCase());
+                                return Observable.just(student,student1,student2);
+                            }
+                        })*/
+
+                        //concat map operator
+
+                        .concatMap(new Function<Student, ObservableSource<?>>() {
+                            @Override
+                            public Observable<Student> apply(Student student) throws Throwable {
+
+                                Student student1 = new Student();
+                                student1.setName("Tom");
+
+                                Student student2 = new Student();
+                                student2.setName("Francis");
+
+                                student.setName(student.getName().toUpperCase());
+                                return Observable.just(student,student1,student2);
+                            }
+                        })
+                        .subscribeWith(getObserver()));
+    }
+
+    private DisposableObserver getObserver(){
+        myObserver = new DisposableObserver<Student>() {
+            @Override
+            public void onNext(@NonNull Student s) {
+
+                Log.e(TAG, s.getName());
+
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e(TAG,"on error invoked");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG,"on complete invoked");
+            }
+        };
+
+        return myObserver;
+    }
+
+    private ArrayList<Student> getStudents(){
+
+        ArrayList<Student> students = new ArrayList<>();
+
+        Student student = new Student();
+        student.setName("Mike");
+        student.setAge(5);
+        student.setEmail("a@mail.com");
+        student.setRegistrationDate("5th March");
+        students.add(student);
+
+        Student student1 = new Student();
+        student1.setName("Mike");
+        student1.setAge(5);
+        student.setEmail("a@mail.com");
+        student1.setRegistrationDate("5th March");
+        students.add(student1);
+
+        Student student2 = new Student();
+        student2.setName("Mike");
+        student2.setAge(5);
+        student.setEmail("a@mail.com");
+        student2.setRegistrationDate("5th March");
+        students.add(student2);
+
+        return students;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
+    }
+}
